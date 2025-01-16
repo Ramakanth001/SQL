bb795354f0b9c3c7f40f73170a2b1992e18abdb5
Ramakanth001 <ramakanthseshabhattar@gmail.com>
2025-01-16 16:20:24 +0800

    strings file added

---
+++ strings_functions.sql
@@ -0,0 +1,22 @@
+-- Concat
+SELECT CONCAT (author_fname, ' ', author_lname, '!!')
+AS author_full_name
+FROM books;
+
+-- Concat WITH SEPERATOR 
+SELECT CONCAT_WS('-',title, author_fname, author_lname) 
+AS author_key
+FROM books;
+
+-- Starts from 3 and goes till end of string
+SELECT SUBSTRING('Rappa', 3)
+AS COL;
+
+-- Starts from 3 and goes till end of string. ans = ''
+SELECT SUBSTR(title, 1, 10)
+AS small_title
+from books;
+
+-- Starts from 3 and goes till end of string. ans = 'ppa'
+SELECT SUBSTRING('Rappa', -3,3)
+AS COL;
