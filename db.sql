0e82b24f5180dcc63277838af7d7d31c7559eae4
Ramakanth001 <ramakanthseshabhattar@gmail.com>
2025-01-16 16:16:10 +0800

    db comamnds file added

---
+++ db.sql
@@ -0,0 +1,11 @@
+-- Creating a new DB
+CREATE DATABASE practice_db;
+
+-- Delete a DB
+DROP DATABASE database_name;
+
+-- select a DB to work
+USE practice_db;
+
+-- select current db
+select DATABASE();
